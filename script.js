76346ddefb96157d65505c765a559c3a517f4b76
davron0816 <davronergawev0816@gmail.com>
2025-03-25 19:47:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// let kun = prompt("bugun qanaqa kun ")
+
+// switch(kun) {
+//     case "dushanba" :
+//         console.log("Bugun haftaning 1 kuni");
+//         break
+        
+//     case "Seshanba":
+//     console.log("Bugun haftaning ikkinchi kuni.");
+//     break
+//     case "Chorshanba" : 
+//     console.log("Bugun haftaning 3 kuni");
+//     break
+
+
+
+    
+//     default:
+//     console.log("Nomalum raqam.");
+
+// }
+
+let admin1 = prompt("Sizni qanday pravangziz bor")
+
+switch (admin1){
+    case "A":
+    console.log("Sizning kichik mashinalr haydashga pravangiz bor");
+    break;
+    case "B":
+    console.log("Sizning velosiped va moto haydashga prabangiz bor");
+    break;
+    case "D":
+        console.log("Sizning Katta mashinalar haydashga pravangiz bor");
+    break;
+    
+
+
+        
+    default:
+    console.log("Nomalum toifa")
+}
